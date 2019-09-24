1dfbc1271376cd107e6a66f57a7c901364c60179
fangkiyudho <fangkiyudho@gmail.com>
2019-09-24 11:07:34 +0700

    BundleActivity

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,18 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    EditText nameInput;
+    TextView teksView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        teksView = findViewById(R.id.text_output);
     }
 
+    public void getData(View view) {
+        teksView.setText("Hello" + nameInput.getText().toString());
+    }
 }
